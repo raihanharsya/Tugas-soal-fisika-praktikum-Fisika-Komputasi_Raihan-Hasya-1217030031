1a84d538c93896de09c7e3479a3aaeae129c046a
raihanharsya <144420955+raihanharsya@users.noreply.github.com>
2023-09-22 12:44:11 +0700

    Add files via upload
    referensi : https://www.scribd.com/doc/308773988/Soal-Fisika

---
+++ Raihan Harsya_1217030031_tugas soal fisika.py
@@ -0,0 +1,12 @@
+#seseorang bermassa 50kg memanjat sebuah pohon mangga hingga ketinggian 4 meter. untuk mencapai ketinggian orang tersebut memerlukan waktu 8 sekon. maka daya yang dibutuhkan orang tersebut agar dapat memanjat phon itu adalah
+
+m = 50 #kg
+h = 4 #m
+t = 8 #s
+g = 10 #m/s^2
+
+# dimana P =w/t, dan w dapat kita uraikan menjadi m.g.h sehingga rumusnya P=m.g.h/t
+w = m*g*h
+
+P=w/t
+print (P,"watt")
